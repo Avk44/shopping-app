9fbf8fea9ab45889b6ef41c88f19298f3753c0ae
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2021-04-17 12:27:57 +0200

    initial commit

---
+++ src/main/java/com/avk44/avkshopping/AvkShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.avk44.avkshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvkShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvkShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avk44/avkshopping/AvkShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avk44.avkshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvkShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
